f26f527e42b134df5137cece1c63d1c665760bd3
unlugir <treedimober@gmail.com>
2025-09-12 11:48:58 +0300

    HealthBar

---
+++ ErrorSurvivor/Assets/_Project/Scripts/HealthBar.cs
@@ -0,0 +1,33 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ErrorSpace
+{
+    public class HealthBar : MonoBehaviour
+    {
+        [Range(sbyte.MinValue, sbyte.MaxValue)] [SerializeField] sbyte health;
+        
+        [SerializeField] private Image mainFill;
+        [SerializeField] private Image secondaryFill;
+        [SerializeField] private Gradient gradient;
+        [SerializeField] private TMP_Text text;
+        
+        private void UpdateBar()
+        {
+            secondaryFill.fillAmount = health >= 0 ? 0 : health / (float)sbyte.MinValue ;
+            mainFill.fillAmount = health >= 0 ? health/(float)sbyte.MaxValue : 0;
+            var color = gradient.Evaluate((float)health / sbyte.MaxValue);
+
+            mainFill.color = color; 
+            secondaryFill.color = color;
+            text.text = health.ToString();
+        }
+        
+        private void OnValidate()
+        {
+            UpdateBar();
+        }
+    }
+}
