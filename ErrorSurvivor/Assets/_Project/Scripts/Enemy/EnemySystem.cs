d9a5f47a21121231dba5cffaaa86ab62952891f4
Marta Vitos <martavitos20@gmail.com>
2025-09-14 12:36:39 +0100

    Spawn outside of camera view

--- ErrorSurvivor/Assets/_Project/Scripts/Enemy/EnemySystem.cs
+++ ErrorSurvivor/Assets/_Project/Scripts/Enemy/EnemySystem.cs
@@ -11,13 +11,13 @@ namespace ErrorSpace
     {
         public static UnityEvent<Enemy> OnDeath { get; private set; } = new();
         [SerializeField] private List<Enemy> enemyPrefabs;
-        [SerializeField] private List<Collider2D> spawnLocations;
         [SerializeField] private Transform spawnRoot;
         //You can add more than 200 as there was 0 lag in WebGl build from yesterday tests.
         //I set 200 as max because it looks too much when you have more than 500 at one time.
         [Range(100, 1000)]
         [SerializeField] private int maxEnemyCount = 200;
         [SerializeField] private int maxEnemyInWaveCount = 50;
+        [SerializeField] private float waveTimeDelay = 10f;
 
         private int currentEnemyCount = 0;
         private bool shouldSpawn = true;
@@ -27,41 +27,45 @@ namespace ErrorSpace
             GenerateEnemies();
         }
 
-        public async void GenerateEnemies()
+        private async void GenerateEnemies()
         {
             while (shouldSpawn)
             {
-                await UniTask.Delay(TimeSpan.FromSeconds(1), ignoreTimeScale: false);
                 GenerateWave();
+                await UniTask.Delay(TimeSpan.FromSeconds(waveTimeDelay), ignoreTimeScale: false);
             }
         }
         
 
-        public void GenerateWave()
+        private void GenerateWave()
         {
             Debug.Log(currentEnemyCount);
             for (int index = 0; index < maxEnemyInWaveCount; index++)
             {
                 if(maxEnemyCount <= currentEnemyCount) return;
-                
-                int randomSpawnerIndex = Random.Range(0, spawnLocations.Count);
             
-                var spawnLocation = RandomPointInBounds(spawnLocations[randomSpawnerIndex].bounds);
-            
-                Enemy spawnedEnemy = Instantiate(enemyPrefabs[Random.Range(0, enemyPrefabs.Count)], spawnLocation, Quaternion.identity, spawnRoot);
+                Enemy spawnedEnemy = Instantiate(enemyPrefabs[Random.Range(0, enemyPrefabs.Count)], GetRandomPointOutsideTheCamera(), Quaternion.identity, spawnRoot);
                 spawnedEnemy.Initialise();
                 
                 spawnedEnemy.OnDeath.AddListener(() => { currentEnemyCount--;});
                 currentEnemyCount++;
             }
         }
-        
-        public static Vector3 RandomPointInBounds(Bounds bounds) {
-            return new Vector3(
-                Random.Range(bounds.min.x, bounds.max.x),
-                Random.Range(bounds.min.y, bounds.max.y),
-                Random.Range(bounds.min.z, bounds.max.z)
-            );
+
+        private Vector3 GetRandomPointOutsideTheCamera()
+        {
+            var randomVector = GetRandomPoint();
+            randomVector.z = 10f; 
+            return Camera.main.ViewportToWorldPoint(randomVector);
+        }
+
+        private Vector3 GetRandomPoint()
+        {
+            float x = Random.Range(-0.8f, 0.8f);
+            float y = Random.Range(-0.8f, 0.8f);
+            if (x >= 0) x += 1;
+            if (y >= 0) y += 1;
+            return new Vector3(x, y);
         }
     }
     
